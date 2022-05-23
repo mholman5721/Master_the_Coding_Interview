847bf748968d8b74c8997d649aeb17212ea91655
mholman5721 <mholman@alumni.uidaho.edu>
2022-05-23 11:50:47 -0600

    reverse_list.py: added video solution
    5-23-2022 11:50 a.m.

--- reverse_list.py
+++ reverse_list.py
@@ -15,53 +15,68 @@ class LinkedList:
         self.length += 1
         return self
     def reverse(self):
-        ref1 = self.head
-        ref2 = self.head
-        if self.length == 1:
-            return self
-        elif self.length == 2:
-            ref2 = ref2.next
-            ref2.next = ref1
-            ref1.next = None
-            self.head = ref2
-            self.tail = ref1
-            return self
-        else:
-            #   e
-            #   t     h
-            # N-*-*-*-* N
-            #       1 2 3
-            #         
-            # change h.next = None
-            # while 3 != None
-            #   change 2.next = 1
-            #   set 1 = 2
-            #   set 2 = 3
-            #   set 3 = 3.next
-            # change 2.next = 1
-            # swap h and t:
-            #   set e = h
-            #   set h = t
-            #   set t = e
-            p1 = self.head
-            p2 = p1.next
-            p3 = p2.next
+        if not self.head.next:
+            return self.head
 
-            # change h.next = None
-            self.head.next = None
-            # while 3 != None
-            while p3 != None:
-                p2.next = p1
-                p1 = p2
-                p2 = p3
-                p3 = p3.next
-            # change 2.next = 1
-            p2.next = p1
-            # swap h and t:
-            temp = self.head
-            self.head = self.tail
-            self.tail = temp
-        pass
+        self.tail = self.head
+        
+        first = self.head
+        second = first.next
+        while second:
+            temp = second.next
+            second.next = first
+            first = second
+            second = temp
+        self.head.next = None
+        self.head = first
+
+        # ref1 = self.head
+        # ref2 = self.head
+        # if self.length == 1:
+        #     return self
+        # elif self.length == 2:
+        #     ref2 = ref2.next
+        #     ref2.next = ref1
+        #     ref1.next = None
+        #     self.head = ref2
+        #     self.tail = ref1
+        #     return self
+        # else:
+        #     #   e
+        #     #   t     h
+        #     # N-*-*-*-* N
+        #     #       1 2 3
+        #     #         
+        #     # change h.next = None
+        #     # while 3 != None
+        #     #   change 2.next = 1
+        #     #   set 1 = 2
+        #     #   set 2 = 3
+        #     #   set 3 = 3.next
+        #     # change 2.next = 1
+        #     # swap h and t:
+        #     #   set e = h
+        #     #   set h = t
+        #     #   set t = e
+        #     p1 = self.head
+        #     p2 = p1.next
+        #     p3 = p2.next
+
+        #     # change h.next = None
+        #     self.head.next = None
+        #     # while 3 != None
+        #     while p3 != None:
+        #         p2.next = p1
+        #         p1 = p2
+        #         p2 = p3
+        #         p3 = p3.next
+        #     # change 2.next = 1
+        #     p2.next = p1
+        #     # swap h and t:
+        #     temp = self.head
+        #     self.head = self.tail
+        #     self.tail = temp
+        return self
     def print(self):
         ptr = self.head
         printString = ""
